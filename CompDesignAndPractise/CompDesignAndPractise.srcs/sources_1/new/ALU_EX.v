e97c049cee3e726511e2769f8a901420e714d7e4
x568059888 <xu568059888@163.com>
2021-07-09 19:47:30 +0800

    通过单周期CPU设计仿真的全部测试样例(不含附加指令).

--- CompDesignAndPractise/CompDesignAndPractise.srcs/sources_1/new/ALU_EX.v
+++ CompDesignAndPractise/CompDesignAndPractise.srcs/sources_1/new/ALU_EX.v
@@ -1,7 +1,7 @@
-`timescale 1ns / 1ps
+`include "param.v"
 
 module ALU_EX(
-    input   [2:0]   alu_op,
+    input   [3:0]   alu_op,
     input   [31:0]  ALU_A,
     input   [31:0]  ALU_B,
     output  [1:0]   compare_result,
@@ -10,8 +10,8 @@ module ALU_EX(
     
     
     assign compare_result = ((ALU_A + ~ALU_B + 1) == 0) ? `EQUAL :
-                            ((ALU_A + ~ALU_B + 1) < 0) ? `LESS :
-                            ((ALU_A + ~ALU_B + 1) > 0) ? `GREATER : `NOP;
+                             ((ALU_A + ~ALU_B + 1) > 'h8000_000) ? `LESS :
+                             ((ALU_A + ~ALU_B + 1) > 0) ? `GREATER : `NOP;
     
     always @ (*) begin
 		case (alu_op)
@@ -31,41 +31,43 @@ module ALU_EX(
 			     ALU_C = ALU_A >> ALU_B[4:0];
 			`SRA:
 			    case(ALU_B[4:0])
-			         0:   ALU_C = ALU_A;
-			         1:   ALU_C = {{1{ALU_C[31]}}, ALU_A[30:0]};
-			         2:   ALU_C = {{2{ALU_C[31]}}, ALU_A[29:0]};
-			         3:   ALU_C = {{3{ALU_C[31]}}, ALU_A[28:0]};
-			         4:   ALU_C = {{4{ALU_C[31]}}, ALU_A[27:0]};
-			         5:   ALU_C = {{5{ALU_C[31]}}, ALU_A[26:0]};
-			         6:   ALU_C = {{6{ALU_C[31]}}, ALU_A[25:0]};
-			         7:   ALU_C = {{7{ALU_C[31]}}, ALU_A[24:0]};
-			         8:   ALU_C = {{8{ALU_C[31]}}, ALU_A[23:0]};
-			         9:   ALU_C = {{9{ALU_C[31]}}, ALU_A[22:0]};
-			         10:  ALU_C = {{10{ALU_C[31]}}, ALU_A[21:0]};
-			         11:  ALU_C = {{11{ALU_C[31]}}, ALU_A[20:0]};
-			         12:  ALU_C = {{12{ALU_C[31]}}, ALU_A[19:0]};
-			         13:  ALU_C = {{13{ALU_C[31]}}, ALU_A[18:0]};
-			         14:  ALU_C = {{14{ALU_C[31]}}, ALU_A[17:0]};
-			         15:  ALU_C = {{15{ALU_C[31]}}, ALU_A[16:0]};
-			         16:  ALU_C = {{16{ALU_C[31]}}, ALU_A[15:0]};
-			         17:  ALU_C = {{17{ALU_C[31]}}, ALU_A[14:0]};
-			         18:  ALU_C = {{18{ALU_C[31]}}, ALU_A[13:0]};
-			         19:  ALU_C = {{19{ALU_C[31]}}, ALU_A[12:0]};
-			         20:  ALU_C = {{20{ALU_C[31]}}, ALU_A[11:0]};
-			         21:  ALU_C = {{21{ALU_C[31]}}, ALU_A[10:0]};
-			         22:  ALU_C = {{22{ALU_C[31]}}, ALU_A[9:0]};
-			         23:  ALU_C = {{23{ALU_C[31]}}, ALU_A[8:0]};
-			         24:  ALU_C = {{24{ALU_C[31]}}, ALU_A[7:0]};
-			         25:  ALU_C = {{25{ALU_C[31]}}, ALU_A[6:0]};
-			         26:  ALU_C = {{26{ALU_C[31]}}, ALU_A[5:0]};
-			         27:  ALU_C = {{27{ALU_C[31]}}, ALU_A[4:0]};
-			         28:  ALU_C = {{28{ALU_C[31]}}, ALU_A[3:0]};
-			         29:  ALU_C = {{29{ALU_C[31]}}, ALU_A[2:0]};
-			         30:  ALU_C = {{30{ALU_C[31]}}, ALU_A[1:0]};
-			         31:  ALU_C = {{31{ALU_C[31]}}, ALU_A[0]};
+			         'd0:   ALU_C = ALU_A;
+			         'd1:   ALU_C = {{1{ALU_A[31]}}, ALU_A[31:1]};
+			         'd2:   ALU_C = {{2{ALU_A[31]}}, ALU_A[31:2]};
+			         'd3:   ALU_C = {{3{ALU_A[31]}}, ALU_A[31:3]};
+			         'd4:   ALU_C = {{4{ALU_A[31]}}, ALU_A[31:4]};
+			         'd5:   ALU_C = {{5{ALU_A[31]}}, ALU_A[31:5]};
+			         'd6:   ALU_C = {{6{ALU_A[31]}}, ALU_A[31:6]};
+			         'd7:   ALU_C = {{7{ALU_A[31]}}, ALU_A[31:7]};
+			         'd8:   ALU_C = {{8{ALU_A[31]}}, ALU_A[31:8]};
+			         'd9:   ALU_C = {{9{ALU_A[31]}}, ALU_A[31:9]};
+			         'd10:  ALU_C = {{10{ALU_A[31]}}, ALU_A[31:10]};
+			         'd11:  ALU_C = {{11{ALU_A[31]}}, ALU_A[31:11]};
+			         'd12:  ALU_C = {{12{ALU_A[31]}}, ALU_A[31:12]};
+			         'd13:  ALU_C = {{13{ALU_A[31]}}, ALU_A[31:13]};
+			         'd14:  ALU_C = {{14{ALU_A[31]}}, ALU_A[31:14]};
+			         'd15:  ALU_C = {{15{ALU_A[31]}}, ALU_A[31:15]};
+			         'd16:  ALU_C = {{16{ALU_A[31]}}, ALU_A[31:16]};
+			         'd17:  ALU_C = {{17{ALU_A[31]}}, ALU_A[31:17]};
+			         'd18:  ALU_C = {{18{ALU_A[31]}}, ALU_A[31:18]};
+			         'd19:  ALU_C = {{19{ALU_A[31]}}, ALU_A[31:19]};
+			         'd20:  ALU_C = {{20{ALU_A[31]}}, ALU_A[31:20]};
+			         'd21:  ALU_C = {{21{ALU_A[31]}}, ALU_A[31:21]};
+			         'd22:  ALU_C = {{22{ALU_A[31]}}, ALU_A[31:22]};
+			         'd23:  ALU_C = {{23{ALU_A[31]}}, ALU_A[31:23]};
+			         'd24:  ALU_C = {{24{ALU_A[31]}}, ALU_A[31:24]};
+			         'd25:  ALU_C = {{25{ALU_A[31]}}, ALU_A[31:25]};
+			         'd26:  ALU_C = {{26{ALU_A[31]}}, ALU_A[31:26]};
+			         'd27:  ALU_C = {{27{ALU_A[31]}}, ALU_A[31:27]};
+			         'd28:  ALU_C = {{28{ALU_A[31]}}, ALU_A[31:28]};
+			         'd29:  ALU_C = {{29{ALU_A[31]}}, ALU_A[31:29]};
+			         'd30:  ALU_C = {{30{ALU_A[31]}}, ALU_A[31:30]};
+			         'd31:  ALU_C = {{31{ALU_A[31]}}, ALU_A[31]};
 			         default:
 			              ALU_C = {32{ALU_C[31]}};
 			    endcase
+			`LUI:
+			     ALU_C = ALU_B;
 			default: ;
 		endcase
 	end
--- example/CPU/CPU.srcs/sources_1/new/IROM.v
+++
@@ -1,9 +0,0 @@
-module IROM(
-    input [31:0] addr,
-    output [31:0] inst
-    );
-    inst_mem imem(
-        .a    (addr[15:2]),
-        .spo  (inst)
-    );
-endmodule
